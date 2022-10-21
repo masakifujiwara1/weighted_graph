b9810220d63ef4816fc4c61225740563d79cdcf5
MasakiFujiwara <fmasaki2020@gmail.com>
2022-10-22 01:32:03 +0900

    success edge sort

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,5 +1,6 @@
 import numpy as np
 import yaml
+import pprint
 
 with open('write_points.yaml', 'r') as yml:
     config_points = yaml.safe_load(yml)
@@ -10,7 +11,7 @@ with open('write_line.yaml', 'r') as yml:
 # print(config_points)
 
 # nodeの数
-print(len(config_points['make_points']))
+# print(len(config_points['make_points']))
 
 # 空のnode用意
 node_l = {}
@@ -26,6 +27,46 @@ node_l_ = {}
 
 # 空のedge用意
 edge = {}
+for i in range(len(config_line['make_line'])):
+    # print(config_line['make_line'][i]['point1']['point_name'])
+    # print(i)
+    # print(edge.get(str(config_line['make_line'][i]['point1']['point_name'])))
 
+    if (edge.get(str(config_line['make_line'][i]['point1']['point_name'])) != None) and (edge.get(str(config_line['make_line'][i]['point2']['point_name'])) != None):
+        edge[str(config_line['make_line'][i]['point1']['point_name'])][str(config_line['make_line'][i]['point2']['point_name'])] = float(config_line['make_line'][i]['distance'])
+        edge[str(config_line['make_line'][i]['point2']['point_name'])][str(config_line['make_line'][i]['point1']['point_name'])] = float(config_line['make_line'][i]['distance'])
 
-print(node_l)
+    elif edge.get(str(config_line['make_line'][i]['point1']['point_name'])) != None:
+            # print("exit!!")
+        edge[str(config_line['make_line'][i]['point1']['point_name'])][str(config_line['make_line'][i]['point2']['point_name'])] = float(config_line['make_line'][i]['distance'])
+        edge[str(config_line['make_line'][i]['point2']['point_name'])] = {str(config_line['make_line'][i]['point1']['point_name']) : float(config_line['make_line'][i]['distance'])}
+        
+    elif edge.get(str(config_line['make_line'][i]['point2']['point_name'])) != None:
+        edge[str(config_line['make_line'][i]['point2']['point_name'])][str(config_line['make_line'][i]['point1']['point_name'])] = float(config_line['make_line'][i]['distance'])
+        edge[str(config_line['make_line'][i]['point1']['point_name'])] = {str(config_line['make_line'][i]['point2']['point_name']) : float(config_line['make_line'][i]['distance'])}
+
+    else:
+        edge[str(config_line['make_line'][i]['point1']['point_name'])] = {str(config_line['make_line'][i]['point2']['point_name']) : float(config_line['make_line'][i]['distance'])}
+        edge[str(config_line['make_line'][i]['point2']['point_name'])] = {str(config_line['make_line'][i]['point1']['point_name']) : float(config_line['make_line'][i]['distance'])}
+
+pprint.pprint(node_l)
+# pprint.pprint(edge)
+
+# print(type(edge))
+
+edge = dict(sorted(edge.items()))
+# edge2 = sorted(edge2.items())
+# edge2 = dict(sorted(edge2.items()))
+
+# 辞書第2階層のソート
+for k, v in edge.items():
+    # print(k, v)
+    if k == '0':
+        # print("success")
+        edge_ = {k : dict(sorted(v.items()))}
+    else:
+        edge_[k] = dict(sorted(v.items()))
+
+# print(edge2)
+# print(type(edge2))
+pprint.pprint(edge_, sort_dicts=False)
