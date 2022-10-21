5345abf12424eb1cb6f3c0afe58ac43ecf3fd168
MasakiFujiwara <fmasaki2020@gmail.com>
2022-10-22 02:40:57 +0900

    successful route restoration

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -2,10 +2,15 @@ import numpy as np
 import yaml
 import pprint
 
-with open('write_points.yaml', 'r') as yml:
+INITIAL_NODE = 4
+GOAL_NODE = 3
+POINT_PATH = 'write_points.yaml'
+LINE_PATH = 'write_line.yaml'
+
+with open(POINT_PATH, 'r') as yml:
     config_points = yaml.safe_load(yml)
 
-with open('write_line.yaml', 'r') as yml:
+with open(LINE_PATH, 'r') as yml:
     config_line = yaml.safe_load(yml)
 
 # print(config_points)
@@ -20,7 +25,7 @@ for i in range(len(config_points['make_points'])):
     node_l[str(config_points['make_points'][i]['id'])] = np.inf
 
 # 初期位置ノード反映(仮：node0)
-node_l['0'] = 0
+node_l[str(INITIAL_NODE)] = 0
 
 # 確定ルート
 node_l_ = {}
@@ -57,6 +62,7 @@ pprint.pprint(node_l)
 edge = dict(sorted(edge.items()))
 # edge2 = sorted(edge2.items())
 # edge2 = dict(sorted(edge2.items()))
+# print(len(edge))
 
 # 辞書第2階層のソート
 for k, v in edge.items():
@@ -70,3 +76,63 @@ for k, v in edge.items():
 # print(edge2)
 # print(type(edge2))
 pprint.pprint(edge_, sort_dicts=False)
+
+prev_list = [0] * 6
+
+# calc route
+while True:
+    # 未確定ノードの中からルートが最小のノードの選択
+    if len(node_l) == 1:
+        min_edge = list(node_l.keys())[0]
+    else:
+        min_edge = min(node_l, key=node_l.get)
+        # print(type(min_edge))
+        # print(min_edge)
+
+    # 探索したノード間のルートの削除
+    del_list = []
+    for i in edge.keys():
+        for j in edge[i].keys():
+            if j == min_edge:
+                del_list.append(i+j)
+                # print(del_list)
+    for i in del_list:
+        del edge[i[0]][i[1]]
+
+    # 探索したノードのコストが以前のものより小さければ更新する
+    for i,j in edge[min_edge].items():
+        if (j + node_l[min_edge]) < node_l[i]:
+            node_l[i] = j + node_l[min_edge]
+            prev_list[int(i)] = int(min_edge)
+
+    # 確定したノードへのルートの削除
+    node_l_[min_edge] = node_l[min_edge]
+    if len(edge) == 1:
+        break
+    del edge[min_edge]
+    del node_l[min_edge]
+
+    print("未確定のノード ", end="")
+    print(node_l)
+    print("確定したノード ", end="")
+    print(node_l_)
+    print("-"*100)
+
+print("確定したノード ", end="")
+print(node_l_)
+
+# print(prev_list)
+# prev_list -> [0, 0, 0, 1, 3, 1]
+route = [GOAL_NODE]
+current_num = GOAL_NODE
+# print(route)
+if current_num != INITIAL_NODE:
+    for i in range(len(config_points['make_points'])):
+        route.append(prev_list[current_num])
+        current_num = prev_list[current_num]
+        if current_num == INITIAL_NODE:
+            route.reverse()
+            break
+
+print("start:" + str(INITIAL_NODE), "goal:" + str(GOAL_NODE), "route:", end="")
+print(route)
